f11013edfdc74fde9810b962258aea32f04a64ca
lengochieu1211-sudo <lengochieu1211@gmail.com>
2026-09-04 10:11:27 +0700

    feat(vxt): retain hanger points and real dimension specs in preview plan

--- incubator/hnl-vxt-pro/src/HNL.VXT.Core/Preview/VxtPreviewPlan.cs
+++ incubator/hnl-vxt-pro/src/HNL.VXT.Core/Preview/VxtPreviewPlan.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using HNL.VXT.Core.Geometry;
 
 namespace HNL.VXT.Core.Preview
 {
@@ -6,6 +7,11 @@ namespace HNL.VXT.Core.Preview
     {
         public List<PreviewLine> Lines { get; } = new List<PreviewLine>();
         public List<PreviewText> Texts { get; } = new List<PreviewText>();
+
+        // WYSIWYG bridge data. These let the AutoCAD renderer use the same Block/DimStyle
+        // that Create will use without polluting Model Space with temporary DB entities.
+        public List<Point2> HangerPoints { get; } = new List<Point2>();
+        public List<PreviewDimension> Dimensions { get; } = new List<PreviewDimension>();
 
         public int MainSegmentCount { get; set; }
         public int FurringSegmentCount { get; set; }
