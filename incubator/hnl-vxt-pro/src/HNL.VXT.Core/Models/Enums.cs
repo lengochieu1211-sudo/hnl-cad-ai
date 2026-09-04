3e577de9b83a416a58b1dc6a504d405bdde043c2
lengochieu1211-sudo <lengochieu1211@gmail.com>
2026-09-04 09:06:38 +0700

    feat(vxt): add legacy parity layout enums for XC XP TY

--- incubator/hnl-vxt-pro/src/HNL.VXT.Core/Models/Enums.cs
+++ incubator/hnl-vxt-pro/src/HNL.VXT.Core/Models/Enums.cs
@@ -1,5 +1,41 @@
 namespace HNL.VXT.Core.Models
 {
+    public enum MainDirectionMode
+    {
+        Horizontal = 0,
+        Vertical = 1,
+        TwoPoints = 2,
+        RectangleRegions = 3,
+        Auto = 4
+    }
+
+    public enum MainLayoutMode
+    {
+        Auto = 0,
+        BalancedTwoEnds = 1,
+        OneSide = 2
+    }
+
+    public enum HangerLayoutMode
+    {
+        BalancedTwoEnds = 0,
+        OneSideFollowFurring = 1
+    }
+
+    public enum BlockTarget
+    {
+        Main = 0,
+        Furring = 1,
+        Hanger = 2
+    }
+
+    public enum EquipmentTarget
+    {
+        General = 0,
+        Main = 1,
+        Furring = 2
+    }
+
     public enum DimensionPosition
     {
         Auto = 0,
