87b6e39c2070627bbc4789cee8504032bfeda780
lengochieu1211-sudo <lengochieu1211@gmail.com>
2026-09-04 21:17:45 +0700

    feat(vxt): persist manual region layout in session

--- incubator/hnl-vxt-pro/src/HNL.VXT.AutoCAD/VxtSession.cs
+++ incubator/hnl-vxt-pro/src/HNL.VXT.AutoCAD/VxtSession.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Autodesk.AutoCAD.DatabaseServices;
 using HNL.VXT.Core.Geometry;
+using HNL.VXT.Core.Layout;
 using HNL.VXT.Core.Models;
 using HNL.VXT.UI.ViewModels;
 
@@ -16,6 +18,9 @@ namespace HNL.VXT.AutoCAD
         public ObjectId[] GeneralEquipmentIds { get; set; } = new ObjectId[0];
         public ObjectId[] MainEquipmentIds { get; set; } = new ObjectId[0];
         public ObjectId[] FurringEquipmentIds { get; set; } = new ObjectId[0];
+
+        // Manual rectangle regions from the V6.7.4 "Quét HCN" workflow.
+        public List<VxtLayoutRegion> Regions { get; } = new List<VxtLayoutRegion>();
 
         public bool HasBoundary => Boundary != null;
     }
