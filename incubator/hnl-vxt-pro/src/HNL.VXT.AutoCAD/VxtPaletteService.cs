5d15ef7b0479018687cd1c0f727a6a5be12bbd3f
lengochieu1211-sudo <lengochieu1211@gmail.com>
2026-09-09 09:39:26 +0700

    HNL VXT: apply compact section order and toggle pass

--- incubator/hnl-vxt-pro/src/HNL.VXT.AutoCAD/VxtPaletteService.cs
+++ incubator/hnl-vxt-pro/src/HNL.VXT.AutoCAD/VxtPaletteService.cs
@@ -21,6 +21,8 @@ namespace HNL.VXT.AutoCAD
                 VxtPaletteCompactTuner.Apply(_view, bridge, _view.ViewModel);
                 VxtPaletteRuntimePolish.Apply(_view);
                 VxtPaletteView.ApplyLayerDimComboThemeFix(_view);
+                VxtPaletteSectionOrder.Apply(_view);
+                VxtPaletteToggleCompact.Apply(_view);
                 VxtSession.Current.ViewModel = _view.ViewModel;
 
                 _palette = new PaletteSet("HNL Tool - VXT Pro v7.0.0-beta.1", PaletteGuid)
