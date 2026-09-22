2455294d52fb876a51fcf5a262349d389c18ae6e
lengochieu1211-sudo <lengochieu1211@gmail.com>
2026-09-22 17:01:18 +0700

    fix(vxt): keep Auto DIM extension legs editable

--- incubator/hnl-vxt-pro/src/HNL.VXT.Core/Preview/VxtPreviewPlanBuilder.cs
+++ incubator/hnl-vxt-pro/src/HNL.VXT.Core/Preview/VxtPreviewPlanBuilder.cs
@@ -651,8 +651,8 @@ namespace HNL.VXT.Core.Preview
             {
                 side = "C-V";
                 var index = GetAndIncrement(stack, side);
-                baseX = (domain.MinX + domain.MaxX) * 0.5;
-                textX = baseX + index * spacing;
+                baseX = sourceBaseCoordinate ?? (domain.MinX + domain.MaxX) * 0.5;
+                textX = baseX + distance + index * spacing;
             }
             else if (position == DimensionPosition.Left || position == DimensionPosition.Bottom)
             {
@@ -703,8 +703,8 @@ namespace HNL.VXT.Core.Preview
             {
                 side = "C-H";
                 var index = GetAndIncrement(stack, side);
-                baseY = (domain.MinY + domain.MaxY) * 0.5;
-                textY = baseY + index * spacing;
+                baseY = sourceBaseCoordinate ?? (domain.MinY + domain.MaxY) * 0.5;
+                textY = baseY + distance + index * spacing;
             }
             else if (position == DimensionPosition.Top || position == DimensionPosition.Left)
             {
